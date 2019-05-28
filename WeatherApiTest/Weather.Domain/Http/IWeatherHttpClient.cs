89cf7b64e074ebb2bfd6257d878dc5719ae0bf85
Pawel Ochman <Pawel.Ochman@mandg.com>
2019-05-28 16:18:55 +0100

    Init commit

---
+++ WeatherApiTest/Weather.Domain/Http/IWeatherHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Weather.Domain.Http
+{
+    public interface IWeatherHttpClient
+    {
+        Task<TResult> GetAsync<TResult>(string url);
+    }
+}
---
+++ WeatherApiTest/Weather.Domain/Http/WeatherHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Weather.Domain.Http
+{
+    public class WeatherHttpClient : IWeatherHttpClient
+    {
+        private static HttpClient _client;
+
+        public WeatherHttpClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string url)
+        {
+            var result = await _client.GetAsync(url);
+            var content = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<TResult>(content);
+        }
+    }
+}
